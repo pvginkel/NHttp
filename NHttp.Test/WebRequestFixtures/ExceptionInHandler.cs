800f80be3b666b0faf16a359941d465225eede76
Pieter van Ginkel <pvginkel@gmail.com>
2012-01-08 10:34:48 +0100

    * Added unit test for exception in request handler.

---
+++ NHttp.Test/WebRequestFixtures/ExceptionInHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using NHttp.Test.Support;
+using NUnit.Framework;
+
+namespace NHttp.Test.WebRequestFixtures
+{
+    [TestFixture]
+    public class ExceptionInHandler : FixtureBase
+    {
+        [Test]
+        [ExpectedException(typeof(WebException))]
+        public void TestExceptionInHandler()
+        {
+            using (var server = new HttpServer())
+            {
+                server.RequestReceived += (s, e) =>
+                {
+                    throw new Exception();
+                };
+
+                server.Start();
+
+                var request = (HttpWebRequest)WebRequest.Create(
+                    String.Format("http://{0}/", server.EndPoint)
+                );
+
+                using (var response = request.GetResponse())
+                using (var stream = response.GetResponseStream())
+                using (var reader = new StreamReader(stream))
+                {
+                    reader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
