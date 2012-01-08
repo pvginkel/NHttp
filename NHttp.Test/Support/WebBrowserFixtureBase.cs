869f12b8a28fc1bb0c5d9547d17dcbdd7bc97357
Pieter van Ginkel <pvginkel@gmail.com>
2012-01-08 08:32:18 +0100

    * Added method to simplify sending upload files through the WebBrowser control.

--- NHttp.Test/Support/WebBrowserFixtureBase.cs
+++ NHttp.Test/Support/WebBrowserFixtureBase.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Forms;
 using NUnit.Framework;
+using mshtml;
 
 namespace NHttp.Test.Support
 {
@@ -41,6 +43,19 @@ namespace NHttp.Test.Support
         protected void RegisterHandler(string path, RequestHandlerCallback callback)
         {
             RegisterHandler(new RequestHandler(path, callback));
+        }
+
+        protected void SetFileUpload(HtmlDocument document, string name, string fileName)
+        {
+            var fileElement = (HTMLInputElement)document.GetElementById(name).DomElement;
+
+            fileElement.focus();
+
+            // The first space is to open the file open dialog. The
+            // remainder of the spaces is to have some messages for
+            // until the open dialog actually opens.
+
+            SendKeys.SendWait("       " + fileName + "{ENTER}");
         }
     }
 }
