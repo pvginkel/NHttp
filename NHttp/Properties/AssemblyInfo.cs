f32a92d1cefa4bc5d8a6cc0dc72cd22b1175e4de
Pieter van Ginkel <pvginkel@gmail.com>
2012-01-08 13:23:01 +0100

    * Cleaned up AssemblyInfo.

--- NHttp/Properties/AssemblyInfo.cs
+++ NHttp/Properties/AssemblyInfo.cs
@@ -1,36 +1,21 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("NHttp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simple asynchronous .NET HTTP server")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyCompany("Pieter van Ginkel")]
 [assembly: AssemblyProduct("NHttp")]
-[assembly: AssemblyCopyright("Copyright © HP 2012")]
+[assembly: AssemblyCopyright("Pieter van Ginkel © 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("86f3c50f-4103-4f12-9992-41d2cc4549f1")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
